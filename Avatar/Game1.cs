80b2c22855552e6574ae5b271f707f84939ca42f
Ilya Karabanov <ilya.karabanov@ftrans.net>
2016-07-08 16:52:26 -0400

    Chapter 2 Complete

--- Avatar/Game1.cs
+++ Avatar/Game1.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Avatar.Components;
+using Avatar.GameStates;
+using Avatar.StateManager;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -12,65 +15,80 @@ namespace Avatar
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        GameStateManager gameStateManager;
+        ITitleIntroState titleIntroState;
+        IMainMenuState startMenuState;
+
+        static Rectangle screenRectangle;
+
+        public SpriteBatch SpriteBatch
+        {
+            get { return spriteBatch; }
+        }
+        public static Rectangle ScreenRectangle
+        {
+            get { return screenRectangle; }
+        }
+        public GameStateManager GameStateManager
+        {
+            get { return gameStateManager; }
+        }
+        public ITitleIntroState TitleIntroState
+        {
+            get { return titleIntroState; }
+        }
+        public IMainMenuState StartMenuState
+        {
+            get { return startMenuState; }
+        }
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            screenRectangle = new Rectangle(0, 0, 1280, 720);
+
+            graphics.PreferredBackBufferWidth = ScreenRectangle.Width;
+            graphics.PreferredBackBufferHeight = ScreenRectangle.Height;
+
+            gameStateManager = new GameStateManager(this);
+            Components.Add(gameStateManager);
+
+            this.IsMouseVisible = true;
+
+            titleIntroState = new TitleIntroState(this);
+            startMenuState = new MainMenuState(this);
+
+            gameStateManager.ChangeState((TitleIntroState)titleIntroState, PlayerIndex.One);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            Components.Add(new Xin(this));
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
---
+++ Avatar/GameStates/BaseGameState.cs
@@ -0,0 +1,37 @@
+﻿using Avatar.StateManager;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avatar.GameStates
+{
+    public class BaseGameState : GameState
+    {
+        #region
+        protected static Random random = new Random();
+        protected Game1 GameRef;
+        #endregion
+
+        #region Constructor Region
+        public BaseGameState(Game game) : base(game)
+        {
+            GameRef = (Game1)game;
+        }
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+        #endregion
+    }
+}
